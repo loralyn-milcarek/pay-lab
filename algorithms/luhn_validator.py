eb48cdffe22858743ddb0615bb1b771bde5261be
loralyn-milcarek <loralyn.milcarek@gmail.com>
2025-11-10 10:29:39 -0500

    change input to string

--- algorithms/luhn_validator.py
+++ algorithms/luhn_validator.py
@@ -1,29 +1,17 @@
-def get_last_digit(num):
-  return num % 10
+def validate_card_number(card_number):
+  digits = card_number.replace(' ', '').replace('-', '')
 
-def drop_last_digit(num):
-  return num // 10
-
-def double_digit(digit):
-  doubled = digit * 2
-  return doubled if doubled < 10 else doubled - 9
-
-def validate_check_digit(test_number):
-  check_digit = get_last_digit(test_number)
-  test_number = drop_last_digit(test_number)
-
+  if not digits.isdigit():
+    return False
+  
+  check_digit = int(digits[-1])
   running_sum = 0
-  should_double = True
 
-  while test_number > 0:
-    last_digit = get_last_digit(test_number)
-    
-    running_sum += double_digit(last_digit) if should_double else last_digit
-
-    test_number = drop_last_digit(test_number)
-    should_double = not should_double
+  for i, digit in enumerate(reversed(digits[:-1])):
+    d = int(digit)
+    running_sum += (d * 2 if d < 5 else d * 2 - 9) if i % 2 == 0 else d
   
   return (running_sum + check_digit) % 10 == 0
 
-print(validate_check_digit(17893729974)) # true
-print(validate_check_digit(17893711114)) # false
+# print(validate_card_number('1789 372 9974')) # true
+# print(validate_card_number('17893711114')) # false
